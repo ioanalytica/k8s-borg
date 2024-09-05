161dc0afca43ec764dc4ed1d1e6f25259d55aed5
Benjamin Stein <bjs@ioanalytica.com>
2024-09-05 13:04:59 +0200

    Change image name

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER_IMAGE=harbor.ioanalytica.com/devops/k3s-borg:latest
+DOCKER_IMAGE=harbor.ioanalytica.com/io/devops/k3s-borg:latest
 docker buildx build --platform linux/amd64 -t ${DOCKER_IMAGE} . --push
 
 # end
