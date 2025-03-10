f9886070f43d152d6a803fa4a4723ae673405a8e
Benjamin Stein <bjs@ioanalytica.com>
2025-03-10 15:43:25 +0100

    Update .gitlab-ci.yml file

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER_IMAGE=harbor.ioanalytica.com/io/devops/k3s-borg:0.9.2
+DOCKER_IMAGE=harbor.ioanalytica.com/io/devops/k3s-borg:0.9.4
 docker buildx build --platform linux/amd64 -t ${DOCKER_IMAGE} . --push
 
 # end
