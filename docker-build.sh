661c0f3add767e4f56e9f34e561e4e08cb50f64f
Benjamin Stein <bjs@ioanalytica.com>
2026-02-16 18:20:24 +0100

    v1.0.0

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER_IMAGE=harbor.ioanalytica.com/io/devops/k3s-borg:0.9.9
+DOCKER_IMAGE=harbor.ioanalytica.com/io/devops/k3s-borg:1.0.0
 docker buildx build --platform linux/amd64,linux/arm64 -t ${DOCKER_IMAGE} . --push
 docker-squash.sh ${DOCKER_IMAGE} --platform linux/amd64,linux/arm64 -t ${DOCKER_IMAGE} --push
 
